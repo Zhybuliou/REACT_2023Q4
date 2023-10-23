76879c29acc6f43aba9804ce2d95c807e9cd7d26
Vitali Zhybuliou <zhybuliou.v.v@gmail.com>
2023-10-24 00:51:26 +0300

    fix:fix

--- react-components/src/App.tsx
+++ react-components/src/App.tsx
@@ -1,6 +1,7 @@
 import './App.css';
 import { PureComponent } from 'react';
 
+console.log('hello');
 export default class App extends PureComponent {
   render() {
     return <div> Hello world!!!</div>;
