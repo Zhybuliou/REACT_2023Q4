a8e36f83fce93ae1492238f6240192434729ecfb
Vitali Zhybuliou <zhybuliou.v.v@gmail.com>
2023-10-23 21:05:36 +0300

    Check Hasky commit

--- react-components/src/App.tsx
+++ react-components/src/App.tsx
@@ -3,6 +3,9 @@ import { PureComponent } from 'react';
 
 export default class App extends PureComponent {
   render() {
-    return <div>Hello world!!!</div>;
+    return <div>
+      
+      
+      Hello world!!!</div>;
   }
 }
