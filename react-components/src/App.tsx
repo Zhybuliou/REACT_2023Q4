90782f6235e36d74178204b16caabe38f012046d
Vitali Zhybuliou <zhybuliou.v.v@gmail.com>
2023-10-23 23:12:05 +0300

    feat: add husky

--- react-components/src/App.tsx
+++ react-components/src/App.tsx
@@ -3,8 +3,7 @@ import { PureComponent } from 'react';
 
 export default class App extends PureComponent {
   render() {
-    console.log('hello           world');
-
-    return <div>          Hello world!!!</div>;
+    console.log('hello world');
+    return <div> Hello world!!!</div>;
   }
 }
