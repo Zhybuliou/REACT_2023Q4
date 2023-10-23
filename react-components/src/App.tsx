b2d7e924050775383b02fb1ac2b56fe7818ed0a4
Vitali Zhybuliou <zhybuliou.v.v@gmail.com>
2023-10-23 21:26:59 +0300

    fix: check husky work

--- react-components/src/App.tsx
+++ react-components/src/App.tsx
@@ -3,6 +3,6 @@ import { PureComponent } from 'react';
 
 export default class App extends PureComponent {
   render() {
-    return                    <div>Hello world!!!</div>;
+    return                          <div>Hello world!!!</div>;
   }
 }
