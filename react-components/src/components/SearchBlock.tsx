9fc831a261aa9c7577d5b10329bed26fdd392a58
Vitali Zhybuliou <zhybuliou.v.v@gmail.com>
2023-10-25 22:05:31 +0300

    feat: add function search charater

--- react-components/src/components/SearchBlock.tsx
+++ react-components/src/components/SearchBlock.tsx
@@ -1,15 +1,47 @@
 import { PureComponent } from 'react';
 
-export default class SearchBlock extends PureComponent {
+type SearchType = {
+  search: string;
+  handleKeyDown: (event: React.KeyboardEvent, value: string) => void;
+  handlerOnClick: (value: string) => void;
+};
+type MySearch = {
+  searchString: string;
+};
+
+export default class SearchBlock extends PureComponent<SearchType, MySearch> {
+  constructor(props: SearchType) {
+    super(props);
+    const { search } = this.props;
+    this.state = {
+      searchString: search,
+    };
+    this.handlerOnChange = this.handlerOnChange.bind(this);
+  }
+
+  handlerOnChange(event: React.ChangeEvent<HTMLInputElement>): void {
+    console.log(event.target.value);
+    this.setState({ searchString: event.target.value });
+  }
+
   render() {
+    const { searchString } = this.state;
+    const { handlerOnClick } = this.props;
     return (
       <div className="search-block">
         <input
           className="search-block-input"
+          name="value"
           type="text"
           placeholder="search character ..."
+          value={searchString}
+          onChange={(e) => this.handlerOnChange(e)}
         />
-        <button type="button" className="search-block-button">
+        <button
+          onClick={() => handlerOnClick(searchString)}
+          type="button"
+          className="search-block-button"
+        >
           Search
         </button>
       </div>
