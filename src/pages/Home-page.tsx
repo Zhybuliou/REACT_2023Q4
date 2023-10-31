1263363895648ee113c3bdc2a272476e5e7ac839
Vitali Zhybuliou <zhybuliou.v.v@gmail.com>
2023-10-31 18:43:23 +0300

    fix: husky fix

--- src/pages/Home-page.tsx
+++ src/pages/Home-page.tsx
@@ -19,10 +19,7 @@ export default function HomePage() {
     await setSearchString(value);
     localStorage.setItem('search', value);
     apiRequest(API_BASE_URL, value).then((data) => setStoreApiResult(data));
-    console.log('check-husky');
-    console.log('check-husky');
-    console.log('check-husky');
-    console.log('check-husky');
+    console.log('hello world');
   };
 
   useEffect(() => {
