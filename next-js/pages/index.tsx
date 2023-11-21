6b09ba374eac3e2a22beb4e5f5ecddae1e5df4dd
Vitali Zhybuliou <zhybuliou.v.v@gmail.com>
2023-11-21 15:47:48 +0300

    feat: add component NotFound.tsx

--- next-js/pages/index.tsx
+++ next-js/pages/index.tsx
@@ -1,5 +1,6 @@
 import ApiPagination from '@/components/ApiPagination'
 import Header from '@/components/Header'
+import PageNotFound from '@/components/NotFound'
 import SearchBlock from '@/components/SearchBlock'
 import SelectCards from '@/components/SelectCards'
 import React from 'react'
@@ -14,6 +15,7 @@ export default function Home() {
       <SearchBlock />
       </div>
       <div className="home-page-content-wrapper">
+      <PageNotFound />
         {/* <div className="home-page-content">
           {!data.count && !isFetching && <PageNotFound />}
           {!!characters.length && !isFetching ? <Cards /> : <Loading />}
