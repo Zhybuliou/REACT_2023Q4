20e96e14e0182ba3ce45cac84d75f7e045759d68
Vitali Zhybuliou <zhybuliou.v.v@gmail.com>
2023-12-02 18:41:42 +0300

    feat: add check image

--- src/pages/FormPage.tsx
+++ src/pages/FormPage.tsx
@@ -1,16 +1,54 @@
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
+import update from 'immutability-helper';
 import { useDispatch } from 'react-redux';
 import { addFormValues } from '../store/sliceFormReducer';
+import userScheme from '../validations/userValidation';
 
 export default function FormPage() {
   const dispatch = useDispatch();
   const formState = useRef(null);
-  function handleSubmit(e: React.FormEvent) {
+
+  const [errors, setErrors] = useState({
+    name: false,
+    age: false,
+    email: false,
+    gender: false,
+    check: false,
+    country: false,
+    image: false,
+    password: false,
+    confirmPassword: false,
+  });
+
+  async function handleSubmit(e: React.FormEvent) {
     e.preventDefault();
     if (formState.current) {
       const myForm = formState.current as HTMLFormElement;
       const formObject = new FormData(myForm);
       const formValue = Object.fromEntries(formObject.entries());
+      const isFormValid = await userScheme.isValid(formValue, {
+        abortEarly: false,
+      });
+
+      if (!isFormValid) {
+        userScheme.validate(formValue, { abortEarly: false }).catch((err) => {
+          const errorsData = err.inner.reduce(
+            (acc: { key: boolean }, error: { path: string }) => {
+              return {
+                ...acc,
+                [error.path]: true,
+              };
+            },
+            {}
+          );
+
+          setErrors((prevErrors) =>
+            update(prevErrors, {
+              $set: errorsData,
+            })
+          );
+        });
+      }
       const image = formValue.image as Blob;
       const { name, age, email, gender, check, country } = formValue;
       dispatch(
@@ -32,21 +70,36 @@ export default function FormPage() {
           >
             <label htmlFor="temp-id">Name :</label>
             <input id="temp-id" type="text" name="name" />
+            <div className="error-message-block">
+              {errors.name && 'Please enter valid name'}
+            </div>
             <label htmlFor="temp-age">Age :</label>
             <input id="temp-age" type="number" name="age" />
+            <div className="error-message-block">
+              {errors.age && 'Please enter valid age'}
+            </div>
             <label htmlFor="temp-email">Email :</label>
             <input id="temp-email" type="email" name="email" />
+            <div className="error-message-block">
+              {errors.email && 'Please enter valid email'}
+            </div>
             <label htmlFor="temp-password">Password :</label>
             <input id="temp-password" type="password" name="password" />
-            <label htmlFor="repeat-password">Repeat password :</label>
+            <div className="error-message-block">
+              {errors.password && 'Please enter valid password'}
+            </div>
+            <label htmlFor="confirmPassword">Repeat password :</label>
             <input
-              id="repeat-password"
+              id="confirmPassword"
               type="password"
-              name="repeat-password"
+              name="confirmPassword"
             />
+            <div className="error-message-block">
+              {errors.confirmPassword && 'Please enter valid confirm password'}
+            </div>
             <fieldset id="temp-gender" className="group-gender">
               <p>Gender : </p> Male
-              <input type="radio" value="Male" name="gender" />
+              <input type="radio" value="Male" name="gender" defaultChecked />
               Female
               <input type="radio" value="Female" name="gender" />
             </fieldset>
@@ -60,6 +113,10 @@ export default function FormPage() {
             </div>
             <label htmlFor="temp-image">Image :</label>
             <input id="temp-image" type="file" name="image" />
+            <div className="error-message-block">
+              {errors.image &&
+                'Please enter valid image < 2MB format .png .jpeg'}
+            </div>
             <label htmlFor="temp-country">Country :</label>
             <input
               id="temp-country"
---
+++ src/validations/userValidation.ts
@@ -0,0 +1,44 @@
+import * as yup from 'yup';
+
+const getCharacterValidationError = (str: string) => {
+  return `You password must have at least 1 ${str} character`;
+};
+const SUPPORTED_FORMATS = ['image/jpg', 'image/jpeg', 'image/png'];
+
+const userScheme = yup.object().shape({
+  name: yup
+    .string()
+    .matches(/^[A-Za-z ]*$/, 'Please enter valid name')
+    .max(40)
+    .required('This field is Required'),
+  age: yup.number().positive('Please enter valid age').nullable(),
+  email: yup
+    .string()
+    .email('Please enter valid email')
+    .required('This field is Required'),
+  password: yup
+    .string()
+    .required('Please enter a password')
+    .min(8, 'Password must have at least 8 characters')
+    .matches(/[0-9]/, getCharacterValidationError('digit'))
+    .matches(/[a-z]/, getCharacterValidationError('lowercase'))
+    .matches(/[A-Z]/, getCharacterValidationError('uppercase')),
+  confirmPassword: yup
+    .string()
+    .required('Please re-type your password')
+    .oneOf([yup.ref('password')], 'Passwords does not match'),
+  image: yup
+    .mixed<File>()
+    .test(
+      'Check size image',
+      'upload file',
+      (value) => !value || (value && value.size <= 1024 * 1024)
+    )
+    .test(
+      'Check format image',
+      'upload file',
+      (value) => !value || (value && SUPPORTED_FORMATS.includes(value.type))
+    ),
+});
+
+export default userScheme;
